908be345f24a1634990afa2d275f52de0dd0936f
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-05-07 15:38:57 -0500

    Env Vars Mapping

--- src/WebStatus.WorkerService/Worker.cs
+++ src/WebStatus.WorkerService/Worker.cs
@@ -3,14 +3,21 @@ namespace WebStatus.WorkerService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            string host = _configuration["host"];
+            string stash = _configuration["stash"];
+            string key = _configuration["key"];
+            string addresses = _configuration["addresses"];
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
