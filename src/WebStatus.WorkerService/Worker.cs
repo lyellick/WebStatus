d00be672110e077d883ccb4970616d7d1f6a33bb
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-05-08 13:44:05 -0500

    Added code for checking url status and showing off JSONStash in a simple way

--- src/WebStatus.WorkerService/Worker.cs
+++ src/WebStatus.WorkerService/Worker.cs
@@ -3,6 +3,7 @@ using JSONStashAPI.CSharp.Models;
 using JSONStashAPI.CSharp.Extensions;
 using Newtonsoft.Json;
 using WebStatus.Common.Models;
+using System.Net;
 
 namespace WebStatus.WorkerService
 {
@@ -10,35 +11,55 @@ namespace WebStatus.WorkerService
     {
         private readonly ILogger<Worker> _logger;
         private readonly IConfiguration _configuration;
+        private Dictionary<string, List<Status>> _statuses;
 
         public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
+            _statuses = new();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            string host = _configuration["host"];
-            string key = _configuration["key"];
-            string stash = _configuration["stash"];
-            string addresses = _configuration["addresses"];
-
-            JSONStash storage = new(host);
-
-            while (!stoppingToken.IsCancellationRequested)
+            while (true) 
             {
-                try
-                {
-                    StashResponse response = await storage.GetStashDataAsync(key, stash);
-                }
-                catch (Exception ex)
-                {
+                string host = _configuration["host"];
+                string key = _configuration["key"];
+                string stash = _configuration["stash"];
+                string addressesToCheck = _configuration["addresses"].ToLower();
 
+                string[] addresses = addressesToCheck.Split(",");
+
+                JSONStash storage = new(host);
+
+                StashResponse data = await storage.GetStashDataAsync(key, stash);
+
+                if (data.TryParseData(out _statuses))
+                {
+                    foreach (string address in addresses)
+                    {
+                        using HttpClient client = new HttpClient();
+                        HttpResponseMessage response = await client.GetAsync(address);
+                        Status status = new() { IsUp = response.IsSuccessStatusCode, Checked = DateTimeOffset.UtcNow };
+
+                        if (!_statuses.ContainsKey(address))
+                            _statuses.Add(address, new());
+
+                        _statuses[address].Add(status);
+                    }
                 }
 
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                string json = JsonConvert.SerializeObject(_statuses);
+
+                long size = data.Metadata.Quota.UsedBytes + (json.Length * sizeof(char));
+
+                if (size > data.Metadata.Quota.MaxBytes)
+                    break;
+                
+                await storage.UpdateStashDataAsync(key, stash, json);
+
+                Thread.Sleep(15000);
             }
         }
     }
